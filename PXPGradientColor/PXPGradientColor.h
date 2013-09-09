0fb9b6b2a6b2c228303066dd8bc6604305f6363c
Louka Desroziers <ldesroziers@Steve-Jobs-iMac.local>
2013-09-09 18:43:47 +0200

    Fixed doc issue with Xcode 5

--- PXPGradientColor/PXPGradientColor.h
+++ PXPGradientColor/PXPGradientColor.h
@@ -35,7 +35,7 @@
 
 
 /** Initializes a newly allocated gradient object with an array of colors.
- * @param colors: An array of NSColor objects representing the colors to use to initialize the gradient. There must be at least two colors in the array. The first color is placed at location 0.0 and the last at location 1.0. If there are more than two colors, the additional colors are placed at evenly spaced intervals between the first and last colors.
+ * @param colors: An array of UIColor objects representing the colors to use to initialize the gradient. There must be at least two colors in the array. The first color is placed at location 0.0 and the last at location 1.0. If there are more than two colors, the additional colors are placed at evenly spaced intervals between the first and last colors.
  *
  * @returns The initialized PXPGradientColor object.
  */
@@ -47,7 +47,7 @@
 
 
 /** Initializes a newly allocated gradient object with the specified colors, color locations, and color space.
- * @param colors: An array of NSColor objects representing the colors to use to initialize the gradient.
+ * @param colors: An array of UIColor objects representing the colors to use to initialize the gradient.
  * @param locations: An array of NSNumber objects initialized using float values. Each value must be in the range 0.0 to 1.0. There must be the same number of locations as are colors in the 'colors' parameter.
  * @param colorSpace: The color space to use for the gradient.
  *
